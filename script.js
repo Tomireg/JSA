32cc603d226fb95ae7eda62b44eb2729fef90378
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-02-10 15:23:44 +0000

    dm test

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 
-document.addEventListener("DOMContentLoaded", init);
-function init() {
-    let navButton = document.getElementById("mybtn");
-    navButton.addEventListener("click", setDarkMode);
-}
-
 function setDarkMode() {
     element.classList.toggle("dark-mode");
 }
