a991d4986a055c3e4bb86295429f04d335bf153c
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-02-11 12:01:13 +0000

    new dark mode test

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+    let darkModeStore = localStorage.getItem('dark-mode');
+
+    if (darkModeStore === null) {
+        localStorage.setItem("dark-mode", false);
+    }
+    if (darkModeStore === "true") {
+        setDarkMode();
+    } else {
+        ;
+    }
+
+    let myButton = document.getElementById("mybtn");
+    myButton.addEventListener("click", clickedDarkMode);
+}
+
+function clickedDarkMode(){
+    toggleDarkMode();
+    setDarkMode();
+}
+
 function setDarkMode() {
     let element = document.body;
     element.classList.toggle("dark-mode");
+}
+
+function toggleDarkMode(){
+    if( localStorage.getItem("dark-mode")==="true" ){
+        localStorage.setItem("dark-mode", false );
+        
+    }else{
+        localStorage.setItem("dark-mode", true);
+    }
 }
