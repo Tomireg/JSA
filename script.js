ae18160949eb5cd7dfde8988387786ae3a3db72c
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-02-11 14:47:47 +0000

    gallery

--- script.js
+++ script.js
@@ -34,3 +34,14 @@ function toggleDarkMode() {
         localStorage.setItem("dark-mode", true);
     }
 }
+
+function upDate(previewPic){
+      old=document.getElementById("image").innerHTML
+      document.getElementById("image").innerHTML=previewPic.alt
+      document.getElementById("image").style.background="url("+previewPic.src+")" ;
+       }
+
+       function unDo(){
+           document.getElementById("image").innerHTML=old
+       document.getElementById("image").style.background="slategrey";    
+       }
