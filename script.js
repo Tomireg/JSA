52c07ca30ac2cea51eef0061048f0bbb69c38ca0
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-02-11 14:48:15 +0000

    formating

--- script.js
+++ script.js
@@ -35,13 +35,13 @@ function toggleDarkMode() {
     }
 }
 
-function upDate(previewPic){
-      old=document.getElementById("image").innerHTML
-      document.getElementById("image").innerHTML=previewPic.alt
-      document.getElementById("image").style.background="url("+previewPic.src+")" ;
-       }
+function upDate(previewPic) {
+    old = document.getElementById("image").innerHTML
+    document.getElementById("image").innerHTML = previewPic.alt
+    document.getElementById("image").style.background = "url(" + previewPic.src + ")";
+}
 
-       function unDo(){
-           document.getElementById("image").innerHTML=old
-       document.getElementById("image").style.background="slategrey";    
-       }
+function unDo() {
+    document.getElementById("image").innerHTML = old
+    document.getElementById("image").style.background = "slategrey";
+}
